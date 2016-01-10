44e47e013dd52dff13c93056f29dcd177b1562e0
PerryAbramowitz <pcwitz@gmail.com>
2016-01-10 18:26:35 -0500

    back to jsonp

--- public/js/zmanimCtrl.js
+++ public/js/zmanimCtrl.js
@@ -11,12 +11,11 @@
     var api =  'https://db.ou.org/zmanim/getCalendarData.php?mode=day&timezone=America/New_York&dateBegin=4/14/2014';
     var lat =  '&lat=40.434890';
     var lng =  '&lng=-79.922075';
-    // var cors = '&callback=JSON_CALLBACK';
+    var cors = '&callback=JSON_CALLBACK';
 
-    // var url = api + lat + lng + cors;
-    var url = api + lat + lng;
+    var url = api + lat + lng + cors;
   
-    $http.get(url).then(function(res) {
+    $http.jsonp(url).then(function(res) {
       $scope.zmanim = res.data.zmanim;
       $scope.candleLighting = res.data.candle_lighting;
     });
